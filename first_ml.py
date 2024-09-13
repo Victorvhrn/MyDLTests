20d4026968e98ce75314f8c14f802abf4e1613a4
victorvhrn <victorhugo.vhrn@gmail.com>
2024-09-13 00:07:36 -0300

    BinNet

--- first_ml.py
+++ first_ml.py
@@ -8,22 +8,24 @@ import torch.optim as optim
 
 
 def to_num(x):
-    x = x.to(dtype=torch.int32)
+    if type(x) != torch.Tensor:
+        x = torch.tensor(x)
     x = x.flip(dims=[-1])
-    powers_of_two = 2 ** torch.arange(x.size(-1))
-    return torch.sum(x * powers_of_two).item()
+    num = 0
+    for i in range(32):
+        num += x[i] * (2**i)
+    return num
 
 
 def to_list(n):
     binary_str = bin(n)[2:]
     binary_str = binary_str.zfill(32)
     binary_list = [float(bit) for bit in binary_str]
-
     return binary_list
 
 
 def real_f(x):
-    num = to_num(x)
+    num = int(to_num(x))
     if num % 7 == 0 and num % 9 == 0:
         return 0.0
     elif num % 7 == 0 or num % 9 == 0:
@@ -36,6 +38,8 @@ num_sequences = 100000
 X_list = [to_list(i) for i in range(num_sequences)]
 X = torch.tensor(X_list)
 Y = torch.tensor([real_f(x) for x in X]).view(-1, 1)
+
+
 split_train = int(0.6 * num_sequences)
 train_x = X[:split_train]
 train_y = Y[:split_train]
@@ -57,15 +61,11 @@ class BinNet(nn.Module):
     def __init__(self, D_in=32, H=32, D_out=1):
         super(BinNet, self).__init__()
         self.linear1 = nn.Linear(D_in, H)
-        self.linear2 = nn.Linear(H, H)
-        self.linear3 = nn.Linear(H, D_out)
-        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(H, D_out)
 
     def forward(self, x):
-        x = self.relu(self.linear1(x))
-        x = self.relu(self.linear2(x))
-
-        return F.sigmoid(self.linear3(x))
+        h_1 = F.sigmoid(self.linear1(x))
+        return F.sigmoid(self.linear2(h_1))
 
 
 def train(model, loss_fn, train_x, train_y, val_x, val_y):
@@ -87,6 +87,10 @@ def test(model, loss_fn, test_x, test_y):
     with torch.no_grad():
         out_test = model(test_x)
         loss_test = loss_fn(out_test, test_y)
+        for i in range(100):
+            print(
+                f"i = {i}, x = {test_x[i]} to_num = {to_num(test_x[i])}, out_test = {out_test[i]}"
+            )
         pred = out_test > 0.5
         acc = (pred == test_y).float().mean()
 
